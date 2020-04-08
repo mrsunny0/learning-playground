8dacfb89ac6490a9db84ac2e8f4e7f7779c55936
George Sun <george.lele.sun@gmail.com>
2020-04-08 12:33:50 -0400

    adding recursive call to obtain file structure with pathfinder

--- python-playground/utils/pathfinder/main.py
+++ python-playground/utils/pathfinder/main.py
@@ -1,6 +1,9 @@
+#%%
 import os
 from contextlib import contextmanager
+from collections import namedtuple
 
+#%%
 @contextmanager
 def change_dir(destination):
     cwd = os.getcwd()
@@ -15,3 +18,44 @@ file_structure = {}
 with change_dir("../..") as nwd:
     # create key value pair of the name and the folder path
     file_structure = { p: os.path.join(nwd, p) for p in os.listdir(nwd)}
+
+#%%
+def return_dirs(directory):
+    with change_dir(directory) as nwd:
+        # d = filter(os.path.isdir, os.listdir(os.getcwd()))
+        sub = [ d for d in os.listdir(nwd) if os.path.isdir(d) and (d[0] != "." and d[0] != "_")]
+        sub_dir = [os.path.join(nwd, s) for s in sub]
+
+    # return both name and full path
+    return sub, sub_dir
+
+#%%
+def generate_namedtuple_tree(d="."):
+    # need to extract basename
+    # all subsequent recursive calls will have a full path name
+    # initial case is when searching for the root path, which contains a . qualifier
+    root_dir = d
+    root = os.path.basename(d) if "." not in d else "root"
+
+    # get child dirs and full paths
+    # note that sub is folder name only, sub_dir is full path
+    sub, sub_dir = return_dirs(d)
+
+    # recrusively search through sub_dir (full path)
+    # generate child namedtuples by calling generate_namedtuple_tree 
+    # as the argument for creating the namedtuple ntuple
+    # namedtuple generation should just contain basename paths
+    if sub:
+        ntuple = namedtuple(root, sub + ["path"])
+        return ntuple(*[generate_namedtuple_tree(s) for s in sub_dir], root_dir)   
+
+    # break point is when there are no more subdirs
+    # this indicates a leaf node (no subdirs), 
+    # then the only folder to consider is the root (context dependent)
+    else:
+        ntuple = namedtuple(root, "path")
+        return ntuple(root_dir)
+
+#%%
+root = generate_namedtuple_tree("./test_file_tree")
+print(root)
