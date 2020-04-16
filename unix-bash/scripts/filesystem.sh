22cfa99525930729a8cfed3d762105ef4848938d
George Sun <george.lele.sun@gmail.com>
2020-04-16 11:16:36 -0400

    adding unix and aws files

---
+++ unix-bash/scripts/filesystem.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILE="/home/vivek/lighttpd.tar.gz"
+basename "$FILE"
+f="$(basename -- $FILE)"
+echo "$f"
+
+FILE="/home/vivek/lighttpd.tar.gz"
+echo ${FILE##*/}
+## another example ##
+url="https://www.cyberciti.biz/files/mastering-vi-vim.pdf"
+echo "${url##*/}"
