fb61553b279b62df9f34b5b618959c5851212904
George Sun <george.lele.sun@gmail.com>
2020-05-03 00:27:39 -0400

    practice login and UI storyboarding

---
+++ ios/playground/App-Login-Bluetooth/App-Login-Bluetooth/Controller/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  App-Login-Bluetooth
+//
+//  Created by George Sun on 5/2/20.
+//  Copyright © 2020 George Sun. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    // elements
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var loginSwitch: UISwitch!
+    @IBOutlet weak var inputEmail: UITextField!
+    @IBOutlet weak var inputPassword: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    var loginBool = true
+    
+    // user defaults
+    let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // retrieve user defaults
+        if let email = defaults.string(forKey: "email"), let password = defaults.string(forKey: "password") {
+            inputEmail.text = email
+            inputPassword.text = password
+        }
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = inputEmail.text, let password = inputPassword.text {
+            let onOff = loginSwitch.isOn
+
+            // login
+            if onOff {
+                Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+                    if let e = error {
+                        print(e)
+                    } else {
+                        self.setUserDefaults(email, password)
+                        self.performSegue(withIdentifier: "toTablePage", sender: self)
+                    }
+                }
+            }
+            // signup
+            else {
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    if let e = error {
+                        print(e)
+                    } else {
+                        self.setUserDefaults(email, password)
+                        self.performSegue(withIdentifier: "toTablePage", sender: self)
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func submitPressed(_ sender: UISwitch) {
+        loginBool = !loginBool
+        
+        // change labels
+        typeLabel.text = loginBool ? "Login" : "Signup"
+        submitButton.titleLabel?.text = loginBool ? "Login" : "Signup"
+        
+        // toggle switch
+        sender.setOn(loginBool, animated: true)
+    }
+    
+    func setUserDefaults(_ email: String, _ password: String) {
+        defaults.set(email, forKey: "email")
+        defaults.set(password, forKey: "password")
+    }
+    
+}
+
---
+++ ios/playground/App-Login-Bluetooth/App-Login-Bluetooth/View/MessageCell.swift
@@ -0,0 +1,25 @@
+//
+//  MessageCell.swift
+//  App-Login-Bluetooth
+//
+//  Created by George Sun on 5/2/20.
+//  Copyright © 2020 George Sun. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
