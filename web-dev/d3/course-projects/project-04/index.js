2eab7074b31c58aab77af805139ab42c83051511
George Sun <george.lele.sun@gmail.com>
2020-06-10 17:00:39 -0400

    building nodes and links using stratify and tree

--- web-dev/d3/course-projects/project-04/index.js
+++ web-dev/d3/course-projects/project-04/index.js
@@ -1,2 +1,30 @@
+/**
+ * Initialize materialize modal
+ */
 const modal = document.querySelector(".modal")
 M.Modal.init(modal)
+
+/**
+ * Form firebase submission
+ */
+const form = document.querySelector("form")
+const name = document.querySelector("#name")
+const parent = document.querySelector("#parent")
+const department = document.querySelector("#department")
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+
+    db.collection("employees").add({
+        name: name.value,
+        parent: parent.value,
+        department: department.value
+    })
+
+    // hide modal, using materialize
+    var instance = M.Modal.getInstance(modal)
+    instance.close(modal)
+
+    // reset form
+    form.reset()
+})
