7929083f8b84b35462e9e02ee0a2a114a18c93b0
George Sun <mr_sunny@mit.edu>
2020-10-23 17:50:42 -0400

    c does not override

--- atmel-studio/Tutorial-10-ADC/Tutorial-10-ADC/main.c
+++ atmel-studio/Tutorial-10-ADC/Tutorial-10-ADC/main.c
@@ -19,13 +19,38 @@ void initADC()
 	ADCSRA |= (1<<ADEN); // automatically uses ADC0 pin
 }
 
-uint16_t readADC() 
+uint16_t readADC(void) 
 {
 	// start conversion
 	ADCSRA |= (1<<ADSC);
 	
 	// return the value stored in this register, 10-bit value by default, stored in 16-bit
 	return ADC; 
+}
+
+// read specific channel
+uint16_t readADCChannel(uint8_t channel) 
+{
+	// modulo to lower 3 bits, so between 0-7
+	// use a bit mask to keep the original lower 3 bits
+	channel = channel & 0b00000111; 
+	
+	// select ADMUX pin
+	/**
+	 * ADC0 - ADC7 go from binary 0000 to 0111
+	 * ADCMUX is offset by 0xfc
+	 */
+	ADMUX = (ADMUX & 0xf8); // this clears the last 3 bits, which is the ADC0-7 pins
+	ADMUX |= channel; // choose the pin from ADC0-7, which is 0000 to 0111
+	
+	// start conversion
+	ADCSRA |= (1<<ADSC); 
+	
+	// wait until conversion is finished
+	while(ADCSRA & (1<<ADSC)); // ADSC is cleared when conversion is finished
+	
+	// return register value
+	return ADC;
 }
 
 void setup() 
@@ -43,7 +68,7 @@ int main(void)
     while (1) 
     {
 		// read data
-		data = readADC();
+		data = readADCChannel(0);
 
 		// convert data
 		sprintf(buffer, "%d", data);
